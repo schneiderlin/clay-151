1bcfa43b1e923c9a87d65c5e6f65e4fb7f0851e0
zihao <positivevector@gmail.com>
2024-09-24 07:38:41 +0800

    reproduce

---
+++ notebooks/some_ns.clj
@@ -0,0 +1,8 @@
+(ns some-ns
+  (:require
+   [scicloj.kindly.v4.kind :as kind]))
+
+(defn add [x y]
+  (+ x y))
+
+(add 1 2)
---
+++ src/demo.clj
@@ -0,0 +1,4 @@
+(require '[scicloj.clay.v2.api :as clay])
+
+(clay/make! {:source-path "notebooks/some_ns.clj"})
+
